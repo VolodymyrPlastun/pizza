c3268d2cce5203d998e3434d2d36b784bd02fd5f
VolodymyrPlastun <vplastun8@gmail.com>
2023-04-30 23:59:53 +0300

    add basename

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { store } from './redux/store';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename='pizza'>
+      <BrowserRouter basename='/pizza'>
         <App />
       </BrowserRouter>
     </Provider>
