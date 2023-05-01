4c2d2f922f0d43d1ed2c6c3e159e449e0541ea57
VolodymyrPlastun <vplastun8@gmail.com>
2023-05-01 18:12:09 +0300

    fix hover

--- src/components/PizzaList/PizzaList.styled.jsx
+++ src/components/PizzaList/PizzaList.styled.jsx
@@ -3,14 +3,14 @@ import styled from 'styled-components';
 
 export const Img = styled(CardMedia)`
   transition: transform 0.2s ease-out;
-  &:hover {
-    transform: scale(1.05);
-  }
 `;
 
 export const PizzaCard = styled(Card)`
   &:hover {
     box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.3);
+  }
+  &:hover Img {
+    transform: scale(1.05);
   }
 `;
 
@@ -19,6 +19,7 @@ export const Box = styled.div`
   flex-direction: column;
   height: 100%;
   justify-content: space-between;
+  overflow: hidden;
 `;
 
 export const BtnBox = styled.div`
